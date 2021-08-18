6150a087fe4f6b85bea268da6ff0854227b8e7b8
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-08-18 08:33:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+""" API initialization script """
+
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -7,22 +9,18 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Models
-# from models import Task
-
-
 @app.route('/')
 def index():
+    """ Index view """
     return "<h1 style='color': red>Hello from flask!</h1>"
 
-@app.route('/task', methods=['POST'])
-def create():
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+    """ Get task view """
     from models import Task
-    content = request.json['content']
-    new_task = Task(content, done=False)
-    db.session.add(new_task)
-    db.session.commit()
-    return 'saved'
+    from schemas import task_schema
+    task = Task.query.get(id)
+    return task_schema.jsonify(task)
 
 if __name__ == '__main__':
     app.run(debug=True)
