10eb464186d965cc3986bdb69036acb9e3aee75d
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-08-18 08:26:06 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
+""" API Config"""
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config(object):
     DEBUG = False
@@ -9,20 +9,16 @@ class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-
 class ProductionConfig(Config):
     DEBUG = False
-
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
