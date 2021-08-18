441dbf5ce853497c5668431658405d422f045007
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-08-18 14:15:20 -0300

    Update app.py and README.md

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Video(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(200))
+    duration = db.Column(db.Integer)
+    quality = db.Column(db.String(2)) # HD, 4K?
+    browser_data = db.Column(db.String(200))
+
+    def __init__(self, url, duration, quality, browser_data):
+        self.url = url
+        self.duration = duration
+        self.quality = quality
+        self.browser_data = browser_data
+
+    def __repr__(self):
+        return f'<Broser DATA {self.browser_data}>'
+
+
 @app.route('/')
 def index():
     """ Index view """
