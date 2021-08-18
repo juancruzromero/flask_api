07e643dc0937e3c2a8f22ec58fb3e2aa1d05f3dc
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-08-18 14:17:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Video(db.Model):
     __tablename__ = 'tasks'
