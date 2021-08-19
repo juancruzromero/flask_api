c222e8938bf2e76151cf6188473cfdf3df4b9388
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-08-18 23:20:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,15 +27,15 @@ class Video(db.Model):
         self.browser_data = browser_data
 
     def __repr__(self):
-        return f'<Broser DATA {self.browser_data}>'
+        return f'<Browser data: {self.browser_data}>'
 
 class VideoSchema(ma.Schema):
     """ Video Schema """
     class Meta:
         fields = ('url', 'duration', 'quality')
 
-task_schema = VideoSchema()
-tasks_schema = VideoSchema(many=True)
+video_schema = VideoSchema()
+videos_schema = VideoSchema(many=True)
 
 # Views:
 @app.route('/')
@@ -43,22 +43,47 @@ def index():
     """ Index view """
     return "<h1 style='color': red>Hello from flask!</h1>"
 
-@app.route('/videos/<id>', methods=['GET'])
-def get_video(id):
-    """ Get video view """
-    task = Video.query.get(id)
-    return task_schema.jsonify(task)
-
 @app.route('/video', methods=['POST'])
-def create():
+def create_video():
     url = request.json['url']
     duration = request.json['duration']
     quality = request.json['quality']
     broser_data = str(request.headers.get('User-Agent')) # Not remote_user xD
-    new_task = Video(url, duration, quality, broser_data)
-    db.session.add(new_task)
+    new_video = Video(url, duration, quality, broser_data)
+    db.session.add(new_video)
     db.session.commit()
-    return task_schema.jsonify(new_task)
-    
+    return video_schema.jsonify(new_video)
+
+@app.route('/videos/<id>', methods=['GET'])
+def get_video(id):
+    """ Get video view """
+    video = Video.query.get(id)
+    return video_schema.jsonify(video)
+
+@app.route('/videos', methods=['GET'])
+def get_videos():
+    all_videos = Video.query.all()
+    result = videos_schema.dump(all_videos)
+    return videos_schema.jsonify(result)
+
+@app.route('/videos/<id>', methods=['PUT'])
+def update_video(id):
+    video = Video.query.get(id)
+    url = request.json['url']
+    duration = request.json['duration']
+    quality = request.json['quality']
+    video.url = url
+    video.duration = duration
+    video.quality = quality
+    db.session.commit()
+    return video_schema.jsonify(video)
+
+@app.route('/videos/<id>', methods=['DELETE'])
+def delete_video(id):
+    video = Video.query.get(id)
+    db.session.delete(video)
+    db.session.commit()
+    return video_schema.jsonify(video)
+
 if __name__ == '__main__':
     app.run(debug=True)
